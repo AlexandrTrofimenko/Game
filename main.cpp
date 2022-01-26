17ecfcbfdd884a65dfa78db99931daf1da36aa69
Alexandr <ARTrofimenko@stud.kantiana.ru>
2022-01-26 20:49:55 +0200

    update

--- main.cpp
+++ main.cpp
@@ -1,7 +1,19 @@
 #include <game.hpp>
+#include <menu.hpp>
+#include <iostream>
 #pragma comment(linker, "/SUBSYSTEM:windows /ENTRY:mainCRTStartup")
 int main()
 {
-	game::ReGame();
+	int height = 600; int width = 900;
+	sf::RenderWindow window(sf::VideoMode(width, height), "BirdIn");
+	while (1)
+	{
+		if (!mn::menu(window))
+			break;
+		int score = 0;
+		game::game(window, height, width, score);
+		if (!mn::Remenu(window, score))
+			break;
+	}
 	return 0;
 }
